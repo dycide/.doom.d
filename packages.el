334f13c758ace901bb2572c5abfd1f9fd46d244f
David Nowotny <dycide@gmail.com>
2023-03-16 23:47:02 +0100

    danke mama

--- packages.el
+++ packages.el
@@ -16,6 +16,9 @@
 (package! clang-format+)
 (package! exec-path-from-shell)
 
+(package! real-auto-save)
+(package! exec-path-from-shell)
+
 ;; To install a package directly from a remote git repo, you must specify a
 ;; `:recipe'. You'll find documentation on what `:recipe' accepts here:
 ;; https://github.com/raxod502/straight.el#the-recipe-format
